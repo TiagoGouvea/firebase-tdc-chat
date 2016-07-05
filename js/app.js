7ad69effc4f3b3195d84e975c24fdae5803c326a
TiagoGouvea <tiago@tiagogouvea.com.br>
2016-07-05 12:34:16 -0300

    Changes during TDC

--- js/app.js
+++ js/app.js
@@ -1,41 +1,39 @@
 $(document).ready(function () {
-    // Usuário autenticado?
-    var loggedUser;
-
-    console.log('ready');
-
-    var provider = new firebase.auth.GithubAuthProvider();
-    provider.addScope('user');
 
     //****************** Auth ******************
+
+    var loggedUser;
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user == undefined || user == null || user.displayName == undefined)
+            return;
+        loggedUser = user;
+        console.log('firebase.auth..onAuthStateChanged > user', user);
+        if (user == null) {
+            $('#div-must-auth').show(400);
+        } else {
+            $('#div-must-auth').hide(200);
+            //loggedIn(user);
+        }
+    });
+
     $('#buttton-auth').click(function () {
+        var provider = new firebase.auth.GithubAuthProvider();
+        provider.addScope('user');
         firebase.auth().signInWithPopup(provider).then(function (result) {
-            // var token = result.credential.accessToken;
-            console.log('firebase.auth > user', result.user);
-            loggedIn(result.user);
+            console.log('firebase.auth.signInWithPopup > user', result.user);
+            //loggedIn(result.user);
         }).catch(function (error) {
             console.log('error', error);
             alert(error.message);
         });
     });
 
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user==undefined || user==null || user.displayName==undefined)
-            return;
-        loggedUser = user;
-        console.log('user', user);
-        if (user == null)
-            $('#div-must-auth').show(400);
-        else {
-            $('#div-must-auth').hide(200);
-            loggedIn(user);
-        }
-    });
 
     function loggedIn(user) {
         writeUserData(user.uid, user.displayName, user.photoURL);
-        startChat();
-        setPresence();
+        // startChat();
+        // setPresence();
     }
 
     function writeUserData(userId, displayName, photoUrl) {
@@ -46,43 +44,54 @@ $(document).ready(function () {
     }
 
     function setPresence() {
-        var presenceRef = firebase.database().ref('users/'+loggedUser.uid+'/connections');
-        var lastOnlineRef = firebase.database().ref('users/'+loggedUser.uid+'/lastOnline');
+        var presenceRef = firebase.database().ref('users/' + loggedUser.uid + '/connections');
+        var lastOnlineRef = firebase.database().ref('users/' + loggedUser.uid + '/lastOnline');
         var connectedRef = firebase.database().ref('.info/connected');
 
-        connectedRef.on('value', function(snap) {
+        connectedRef.on('value', function (snap) {
             setSendEnabled(snap.val() === true);
             if (snap.val() === true) {
-                console.log('connectedRef.on > value','conected');
+                console.log('connectedRef.on > value', 'conected');
                 var con = presenceRef.push(true);
                 con.onDisconnect().remove();
                 lastOnlineRef.onDisconnect().set(firebase.database.ServerValue.TIMESTAMP);
-            } else if (snap.val()===false){
-                console.log('connectedRef.on > value','disconected');
+            } else if (snap.val() === false) {
+                console.log('connectedRef.on > value', 'disconected');
             }
         });
-    }
 
+        $('#button-users').show();
+        $('#button-users').click(function () {
+            if (usersRef == undefined)
+                listenUsers();
+            $('#div-users').toggle(200);
+            $('#div-chat').toggle(200);
+        });
+    }
 
     //****************** Messages ************************
     var messagesRef;
 
     function startChat() {
         $('#div-chat').show();
-        $('#button-users').show();
+        $('#button-send').click(function () {
+            sendMessage();
+        });
+        $(document).keypress(function (e) {
+            if (e.which == 13)
+                sendMessage();
+        });
 
-        // Get messages
-        if (messagesRef!=undefined)
+        if (messagesRef != undefined)
             return;
 
         messagesRef = firebase.database().ref('messages/');
 
-        messagesRef.limitToFirst(1).on('value',function (snapshot) {
-            console.log('messagesRef.limitToFirst(1).on > value', snapshot.val());
-        });
+        setSendEnabled(true);
+        return;
 
-        function addMessage(key,message) {
-            if ($('#li-message-'+key).length) return;
+        function addMessage(key, message) {
+            if ($('#li-message-' + key).length) return;
             var template = $('#message-template').html();
             template = template.replace('{key}', key);
             template = template.replace('{user-name}', message.userDisplayName);
@@ -93,46 +102,41 @@ $(document).ready(function () {
         }
 
         function delMessage(key) {
-            $('#li-message-'+key).hide(1000);
+            $('#li-message-' + key).hide(1000);
         }
+
+        messagesRef.limitToFirst(1).on('value', function (snapshot) {
+            console.log('messagesRef.limitToFirst(1).on > value', snapshot.val());
+        });
 
         messagesRef.on('child_added', function (data) {
             console.log('messagesRef.on > child_added', data.val());
-            addMessage(data.key,data.val());
+            addMessage(data.key, data.val());
         });
         messagesRef.on('child_removed', function (data) {
             console.log('messagesRef.on > child_removed', data);
             delMessage(data.key);
         });
-
-        $('#button-send').click(function () {
-            sendMessage();
-        });
-
-        $(document).keypress(function(e) {
-            if(e.which == 13)
-                sendMessage();
-        });
     }
 
     function sendMessage() {
         var message = $('#input-message').val();
-        if (message.trim()=='') return;
+        if (message.trim() == '') return;
         $('#input-message').attr("disabled", true);
         var data = {
             userDisplayName: loggedUser.displayName,
-            userPhotoUrl : loggedUser.photoURL,
+            userPhotoUrl: loggedUser.photoURL,
             uid: loggedUser.uid,
             timestamp: firebase.database.ServerValue.TIMESTAMP,
             body: message
         };
-        var newMessageKey = firebase.database().ref('messages/').push(data,function(){
-            $('#input-message').attr("disabled", false);
-            $('#input-message').val('');
-            $('#input-message').focus();
+        var newMessageKey = firebase.database().ref('messages/').push(data, function (data) {
+            console.log('database.ref.push >',data);
         });
+        $('#input-message').attr("disabled", false);
+        $('#input-message').val('');
+        $('#input-message').focus();
     }
-
 
 
     function setSendEnabled(enabled) {
@@ -146,21 +150,22 @@ $(document).ready(function () {
     var usersRef;
 
     function listenUsers() {
-        function addUser(key,user) {
+
+        function addUser(key, user) {
             var template = $('#user-template').html();
             template = template.replace('{key}', key);
-            template = template.replace('{class}','');
+            template = template.replace('{class}', '');
             template = template.replace('{user-name}', user.displayName);
             template = template.replace('{photo-url}', user.photoUrl);
             $('#ul-users-online').prepend(template);
-            setUserStatus(key,user);
+            setUserStatus(key, user);
         }
 
-        function setUserStatus(key,user) {
-            if (user.lastOnline!=undefined){
-                $('#li-user-'+key).removeClass('online').addClass('offline');
+        function setUserStatus(key, user) {
+            if (user.lastOnline != undefined) {
+                $('#li-user-' + key).removeClass('online').addClass('offline');
             } else {
-                $('#li-user-'+key).removeClass('offline').addClass('online');
+                $('#li-user-' + key).removeClass('offline').addClass('online');
             }
         }
 
@@ -168,19 +173,31 @@ $(document).ready(function () {
 
         usersRef.on('child_added', function (data) {
             console.log('usersRef.on > child_added', data.val());
-            addUser(data.key,data.val());
+            addUser(data.key, data.val());
         });
         usersRef.on('child_changed', function (data) {
             console.log('usersRef.on > child_changed', data.val());
-            setUserStatus(data.key,data.val());
+            setUserStatus(data.key, data.val());
         });
     }
 
-    $('#button-users').click(function () {
-        if (usersRef==undefined)
-            listenUsers();
-        $('#div-users').toggle(200);
-        $('#div-chat').toggle(200);
+
+    //**************** About ********************
+    var dialog = document.querySelector('dialog');
+    var showDialogButton = document.querySelector('#a-about');
+    if (! dialog.showModal) {
+        dialogPolyfill.registerDialog(dialog);
+    }
+    console.log($('.about'));
+    $('.about').click(function(e){
+        e.preventDefault();
+        dialog.showModal();
     });
+    dialog.querySelector('.close').addEventListener('click', function() {
+        dialog.close();
+    });
+
+
+
 
 });
